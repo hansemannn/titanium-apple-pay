ed2fcc3da7aab7767007d31ec915be4c9961432a
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2015-11-27 14:35:21 -0800

    Cleanup 2

--- iphone/build/ti.applepay.build/Release-iphonesimulator/ti.applepay.build/Objects-normal/i386/TiApplepayModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/hans/Documents/appcelerator_modules/ti.applepay/iphone/Classes/TiApplepayModuleAssets.m \
-  /Users/hans/Documents/appcelerator_modules/ti.applepay/iphone/Classes/TiApplepayModuleAssets.h
